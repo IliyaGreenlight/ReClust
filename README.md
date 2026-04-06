d5bd7837a0dab918da4d176b70565566e1e5c449
IliyaGreenlight <ilya.greenlight@gmail.com>
2026-04-07 00:08:19 +0200

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This program helps to clusterize youtube comments using sentence transforming. Baseline is ready, but work is still in progress
+This program helps to clusterize youtube comments using sentence transforming. To test it you need to clone the repository, configure your google APi to download youtube comments and start the app.py
--- mlflow_test.py
+++ mlflow_test.py
@@ -1,11 +1,16 @@
 import mlflow
 import json
 import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from Clusterize import (
     comment_flattening,
     data_prep,
     data_clustering_tsne,
+)
+
+from Visualize import (
     make_dataframe_and_clusters,
     plot_clusters
 )
@@ -65,32 +70,83 @@ def optimize_perplexity(
 
     return best_result
 
+def extract_topic(model_name, comments):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        trust_remote_code=True,
+        dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+        device_map="auto"
+    )
+    prompt = f"""
+       Context:
+       You are given several YouTube comments from the same discussion topic. 
+       Comments are provided in the following format: ["comment 1", "comment 2", "comment 3", "comment 4", "comment 5"]
+       Youtube video has the title: All Best Cat Memes
+
+       Comments:
+       {comments}
+
+       Task:
+       Return a concise topic label (1 to 4 words)
+       No punctuation.
+       No explanation.
+       Only the label.
+       """.strip()
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+
+    with torch.no_grad():
+        outputs = model.generate(
+            **inputs,
+            max_new_tokens=10,
+            temperature=0.2,
+            do_sample=False
+        )
+
+    result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return result
+
+def optimize_model():
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("model_optimization")
+
+    with open("data/cluster_representatives.json", "r", encoding="utf-8") as f:
+        cluster_representatives = json.load(f)
+
+    MODELS = [
+        "Qwen/Qwen2.5-3B-Instruct",
+        "Qwen/Qwen2.5-1.5B-Instruct",
+        "meta-llama/Llama-2-7b-chat-hf",
+        "mistralai/Mistral-7B-Instruct-v0.3",
+        "bigscience/bloomz-7b1-mt"
+    ]
+
 
 def main():
-    EPS = 1.5
-    MIN_SAMPLES = 5
-
-    PERPLEXITY_GRID = [5, 10, 20, 30, 40, 50, 70, 100]
-
-    entries = comment_flattening("data/comments_with_embeddings.json")
-    embeddings, clean_entries = data_prep(entries)
-
-    best = optimize_perplexity(
-        embeddings=embeddings,
-        clean_entries=clean_entries,
-        eps=EPS,
-        min_samples=MIN_SAMPLES,
-        perplexity_values=PERPLEXITY_GRID
-    )
-
-    df, clusters = make_dataframe_and_clusters(
-        best["emb_2d"],
-        best["labels"],
-        clean_entries
-    )
-
-    plot_clusters(df, clusters)
-
+    # EPS = 1.5
+    # MIN_SAMPLES = 5
+    #
+    # PERPLEXITY_GRID = [5, 10, 20, 30, 40, 50, 70, 100]
+    #
+    # entries = comment_flattening("data/comments_with_embeddings.json")
+    # embeddings, clean_entries = data_prep(entries)
+    #
+    # best = optimize_perplexity(
+    #     embeddings=embeddings,
+    #     clean_entries=clean_entries,
+    #     eps=EPS,
+    #     min_samples=MIN_SAMPLES,
+    #     perplexity_values=PERPLEXITY_GRID
+    # )
+    #
+    # df, clusters = make_dataframe_and_clusters(
+    #     best["emb_2d"],
+    #     best["labels"],
+    #     clean_entries
+    # )
+    #
+    # plot_clusters(df, clusters)
+    optimize_model()
 
 if __name__ == "__main__":
     main()
